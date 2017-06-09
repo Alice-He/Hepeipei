ba3992fcc1468b12595ec2cd8fb46a2cd2dd4102
Alices1 <948807264@qq.com>
2017-06-10 00:36:13 +0800

    Create config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -3,6 +3,6 @@ header("Content-type:text/html;charset=utf-8");
 session_start();
 define('DB_HOST','localhost');
 define('DB_USER','root');
-define('DB_PASSWORD','fenghuayu0528');
+define('DB_PASSWORD','');
 define('DB_DATABASE','serct');
 define('DB_PORT',3306);
