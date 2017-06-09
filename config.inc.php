5314c509bca21008d064a2d8dc74277d08dbdf32
Alices1 <948807264@qq.com>
2017-06-09 14:47:08 +0800

    Delete config.inc.php

--- config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Content-type:text/html;charset=utf-8");
-session_start();
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASSWORD','root');
-define('DB_DATABASE','serct');
-define('DB_PORT',3306);
